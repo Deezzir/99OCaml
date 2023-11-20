cea0d3b1f5566b2916006a6bfb9bb55ded6886b3
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2023-11-20 16:40:42 -0500

    Add solution for Problem 02: last_two

--- solutions.ml
+++ solutions.ml
@@ -8,3 +8,15 @@ let rec last (xs : 'a list): 'a option =
 
 assert(last ["a" ; "b" ; "c" ; "d"] = Some "d");;
 assert(last [] = None);;
+
+(* Problem 02. Find the last but one (last and penultimate) elements of a list. (easy) *)
+
+let rec last_two (xs: 'a list): ('a * 'a) option = 
+  match xs with
+  | [] | [_] -> None
+  | [x; y] -> Some(x, y)
+  | _ :: rest -> last_two rest
+;;
+
+assert(last_two ["a"; "b"; "c"; "d"] = Some ("c", "d"));;
+assert(last_two ["a"] = None);;
